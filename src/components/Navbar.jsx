3040d386a309db25104b834ca322dcae85efb003
Azure <paramasuraqutay@gmail.com>
2025-12-20 10:46:42 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Navbar_Link } from '../constants'
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Logo" />
+
+                <ul>
+                    {Navbar_Link.map((link) => (
+                        <li key={link.label}>
+                            <a href="">{link.label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='flex-center gap-3'>
+                    <button>
+                        <img src="/search.svg" alt="" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
